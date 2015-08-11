b7081b79d091d325e72100f7eef39c73661ab932
Rachel Schifano <rschifa1@gmail.com>
2015-08-10 23:12:29 -0500

    Add comments remove println tests

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -16,9 +16,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Sets the delegate of the current UIImagePickerController object to the view controller this is written in.
         imagePicker.delegate = self
     }
 
+    /**
+        Allows user to select a photo from the PhotoLibrary when the Album button is tapped.
+    
+        :param: sender The toolbar Album button
+    */
     @IBAction func imagePickerAlbumButtonTapped(sender: UIButton) {
         imagePicker.allowsEditing = false
         imagePicker.sourceType = .PhotoLibrary
@@ -26,6 +32,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         presentViewController(imagePicker, animated: true, completion: nil)
     }
     
+    /**
+        Allows user to take a photo using either a front or rear camera.
+    
+        :param: sender The toolbar camera button
+    */
     @IBAction func imagePickerCameraButtonTapped(sender: AnyObject) {
         imagePicker.allowsEditing = false
         
@@ -35,8 +46,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             if UIImagePickerController.isCameraDeviceAvailable(UIImagePickerControllerCameraDevice.Front)  {
                 imagePicker.sourceType = .Camera
                 imagePicker.cameraDevice = .Front
-            
-                println("Front camera is available") // TEST
                 
                 presentViewController(imagePicker, animated: true, completion: nil)
 
@@ -44,8 +53,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 // Rear camera available
                 imagePicker.sourceType = .Camera
                 imagePicker.cameraDevice = .Rear
-                
-                println("Rear camera is available") // TEST
                 
                 presentViewController(imagePicker, animated: true, completion: nil)
             }
@@ -55,12 +62,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             let alert = UIAlertController(title: "No camera found", message: "Did not find a camera on your current device", preferredStyle: .Alert)
             alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {(alert: UIAlertAction!) in println("Foo")}))
             presentViewController(alert, animated: true, completion: nil)
-                
-            println("Camera is not available") // TEST
         }
-        // Do I need to dismissViewController here? Maybe
+        // TODO: Figure out if I need to dismissViewController here
     }
     
+    /**
+        Sets the picked image to the image view.
+    
+        :param: picker The current UIImagePickerController
+        :param: info The dictionary which takes a key and returns the original image picked
+    */
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject: AnyObject]) {
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             // Set the picked image in the UIImageView
@@ -70,6 +81,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    /**
+        Dismisses the current view controller.
+    
+        :param: picker The current UIImagePickerController
+    */
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
