28ff51332d6ff1769ade1a8357938327cb4111b1
Rachel Schifano <rschifa1@gmail.com>
2015-08-17 14:23:45 -0500

    Access the shared model

---
+++ MemeMe/MemeMe/MemeTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe
+//
+//  Created by Rachel Schifano on 8/17/15.
+//  Copyright (c) 2015 schifano. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class MemeTableViewController: UITableViewController {
+    
+    // Create array of Meme objects
+    var memes: [Meme]!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the shared model
+        let object = UIApplication.sharedApplication().delegate
+        let appDelegate = object as! AppDelegate
+        memes = appDelegate.memes
+    }
+}
